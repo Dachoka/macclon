9aa8ccd0f0e31764b677af28e6ba778c5b29bec6
Machoka Daisy <machokadaisy25@gmail.com>
2020-10-17 17:58:46 +0300

    add model for images

--- gram/models.py
+++ gram/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    image = models.ImageField(upload_to = 'photos/')
+    name = models.CharField(max_length = 80)
+    caption = models.TextField()
+    profile = models.ForeignKey('Profile', on_delete=models.CASCADE)
+    likes = models.IntegerField()
+    comments = models.TextField()
+    upload_date = models.DateTimeField(auto_now_add=True, null = True)
+
+
