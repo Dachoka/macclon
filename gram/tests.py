39593d56048400158187b95b29e794263528fec4
Machoka Daisy <machokadaisy25@gmail.com>
2020-10-18 11:57:21 +0300

    add test to check if profile objects are instantiated correctly

--- gram/tests.py
+++ gram/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import Image,Profile
 
 # Create your tests here.
+class ProfileTestCase(TestCase):
+    '''
+    Test case that runs test for the profile model objects
+    '''
+
+    def setUp(self):
+       self.daisy = Profile(photo ='photos/dpic.jpeg', bio='Loving Life' )
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.daisy, Profile))
