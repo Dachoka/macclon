14bde83b026a733f8559d6e6c9aac053c1a4b612
Machoka Daisy <machokadaisy25@gmail.com>
2020-10-18 13:14:09 +0300

    add a test to check if image objects are saved correctly

--- gram/tests.py
+++ gram/tests.py
@@ -44,4 +44,8 @@ class ImageTestCase(TestCase):
 
     def test_instance(self):
         self.assertTrue(isinstance(self.photoshoot, Image))
-        
+
+    def test_save_method(self):
+        self.photoshoot.save_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images)>0)
