c0f1705552be8d084581d61fa29346784b9b122f
Machoka Daisy <machokadaisy25@gmail.com>
2020-10-18 11:39:47 +0300

    add a method to save a profile

--- gram/models.py
+++ gram/models.py
@@ -4,13 +4,16 @@ from django.db import models
 class Image(models.Model):
     image = models.ImageField(upload_to = 'photos/')
     name = models.CharField(max_length = 80)
-    caption = models.TextField()
+    caption = models.TextField(blank = True)
     profile = models.ForeignKey('Profile', on_delete=models.CASCADE)
-    likes = models.IntegerField()
-    comments = models.TextField()
+    likes = models.IntegerField(default = 0)
+    comments = models.TextField(blank = True)
     upload_date = models.DateTimeField(auto_now_add=True, null = True)
 
 
 class Profile(models.Model):
     photo = models.ImageField(upload_to = 'photos/')
     bio = models.TextField()
+
+    def save_profile(self):
+        self.save()
