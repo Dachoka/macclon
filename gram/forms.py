15c9be971fa5d56a2076f9c8eae9eebf2de09814
Machoka Daisy <machokadaisy25@gmail.com>
2020-10-19 17:21:33 +0300

    add a form to upload new images

--- gram/forms.py
+++ gram/forms.py
@@ -1,10 +1,18 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Image
 
 
 class UserCreateForm(UserCreationForm):
+    email = forms.EmailField(max_length = 100, help_text = 'Required')
     class Meta:
         model = User
         fields = ['username','email', 'password1','password2']
+
+class UploadPhotoForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        exclude = ['profile','likes','comments','upload_date']
+
 
