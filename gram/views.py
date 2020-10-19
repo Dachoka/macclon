c5c6f8f5ddcd22358571854d38a4a347b4540e71
Machoka Daisy <machokadaisy25@gmail.com>
2020-10-19 07:20:26 +0300

    add a function to register a new user

--- gram/views.py
+++ gram/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from .forms import UserCreateForm
+from .email import send_welcome_email
 
 # Create your views here.
+def homepage(request):
+    return render(request, 'home.html')
+
+def signup(request):
+    form = UserCreateForm()
+
+    if request.method == 'POST':
+        form = UserCreateForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    return render(request, 'registration/signup.html',{"form":form,})
+
