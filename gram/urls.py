8c85133a3fec88e60691ecae85ed893dcf783562
Machoka Daisy <machokadaisy25@gmail.com>
2020-10-20 19:13:19 +0300

    add a route to render profile page

--- gram/urls.py
+++ gram/urls.py
@@ -6,7 +6,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.homepage, name = 'home'),
     path('signup/', views.signup, name = 'signup'),
-    path('upload/',views.uploadphoto, name ='upload')
+    path('upload/',views.uploadphoto, name ='upload'),
+    path('profile/', views.profile, name = 'profile')
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
