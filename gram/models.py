d31bc9d4e66f1f35ae09867d5d9bd9a9a9e26909
Machoka Daisy <machokadaisy25@gmail.com>
2020-10-20 15:29:20 +0300

    update models to use the user model and pyuploadcare

--- gram/models.py
+++ gram/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from pyuploadcare.dj.models import ImageField
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Image(models.Model):
     image = ImageField(blank=True, manual_crop="")
     name = models.CharField(max_length = 80)
     caption = models.TextField(blank = True)
-    profile = models.ForeignKey('Profile', on_delete=models.CASCADE)
+    profile = models.ForeignKey(User, on_delete=models.CASCADE)
     likes = models.IntegerField(default = 0)
     comments = models.TextField(blank = True)
     upload_date = models.DateTimeField(auto_now_add=True, null = True)
@@ -14,22 +15,32 @@ class Image(models.Model):
     def save_image(self):
         self.save()
 
-    def update_image(self):
-        self.update()
+    def update_caption(self, new_caption):
+        self.update(caption = new_caption)
 
     def delete_image(self):
         self.delete()
 
+    @classmethod
+    def get_all_images(cls):
+        all_images = cls.objects.all()
+        return all_images
+
+
 
 class Profile(models.Model):
+    owner = models.OneToOneField(User,on_delete = models.CASCADE, null = True)
     photo = ImageField(blank=True, manual_crop="")
     bio = models.TextField()
 
     def save_profile(self):
         self.save()
 
-    def update_profile(self):
-        self.update()
+    def update_profile(self, new_bio):
+        self.update(bio = new_bio)
 
     def delete_profile(self):
         self.delete()
+
+    def __str__(self):
+        return self.owner.username
